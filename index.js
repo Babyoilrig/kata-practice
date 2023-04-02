301666a5d346b28f5a01b395e784da731dda993f
babyoilrig <catrin.mentzoni@yahoo.com>
2023-04-02 12:45:39 +0100

    Another kata done

--- index.js
+++ index.js
@@ -48,3 +48,17 @@
     //     return "No";
     //   }
     // }
+
+    //Kata 8 kyu - Convert a Number to a String!
+    //DESCRIPTION:
+// We need a function that can transform a number (integer) into a string.
+
+// What ways of achieving this do you know?
+
+// Examples (input --> output):
+// 123  --> "123"
+// 999  --> "999"
+// -100 --> "-100"
+// Solution: function numberToString(num) {
+//     return num.toString();
+//   }
