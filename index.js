275fc181f771b5b389813a29c9d3599b7fbeccde
babyoilrig <catrin.mentzoni@yahoo.com>
2023-04-02 13:17:42 +0100

    Another kata completed

--- index.js
+++ index.js
@@ -76,3 +76,16 @@
 // function opposite(number) {
 //     return number * -1;
 //   }
+
+//Kata - 8 kyu - Remove First and Last Character
+//DESCRIPTION: It's pretty straightforward. Your goal is to create a 
+// function that removes the first and last characters of a string. 
+// You're given one parameter, the original string. You don't have to worry 
+// with strings with less than two characters.
+//Solution: 
+// //function removeChar(str){
+//     let newStr = str.replace(str[0], '');
+//     newStr = newStr.replace(newStr[newStr.length-1], '');
+//   return newStr
+//   };
+  
